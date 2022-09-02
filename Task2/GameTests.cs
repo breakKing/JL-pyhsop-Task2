f3d5ff25b063532bab1e86bead06b4a7d04f2e12
Daniil-Laptop <dduhovny26@gmail.com>
2022-09-03 00:29:04 +0300

    Тест, когда точно такой же offset есть в массиве

--- Task2/GameTests.cs
+++ Task2/GameTests.cs
@@ -60,6 +60,25 @@ public class GameTests
         // Assert
         score.Should().Be(new Score(0, 0));
     }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(50)]
+    [InlineData(1000)]
+    [InlineData(12500)]
+    [InlineData(27632)]
+    public void GetScore_ShouldReturnCorrectScore_WhenOffsetIsCorrectAndPresentedInGameStampsArray(int offset)
+    {
+        // Arrange
+        var stamps = TestDataGenerator.GenerateStampsWithDefinedOffsets(_rand, offsets: offset);
+        var game = new Game(stamps);
+
+        // Act
+        var score = game.getScore(offset);
+
+        // Assert
+        score.Should().Be(stamps.FirstOrDefault(s => s.offset == offset).score);
+    }
 }
 
 public class TestDataGenerator
@@ -85,6 +104,46 @@ public class TestDataGenerator
                 maxOffsetStep,
                 scoreChangeProbability,
                 homeScoreProbability);
+        }
+
+        return gameStamps;
+    }
+
+    public static GameStamp[] GenerateStampsWithDefinedOffsets(Random rand,
+        int count = 50000,
+        int maxOffsetStep = 3,
+        double scoreChangeProbability = 0.0001,
+        double homeScoreProbability = 0.45,
+        params int[] offsets)
+    {
+        if (count <= 0)
+        {
+            return new GameStamp[0];
+        }
+
+        var definedOffsets = offsets.Where(off => off >= 0)
+            .OrderBy(off => off)
+            .ToArray();
+
+        var gameStamps = new GameStamp[count];
+        gameStamps[0] = new GameStamp(0, 0, 0);
+
+        var definedOffsetsCurrentIndex = (definedOffsets[0] == 0) ? 1 : 0;
+
+        for (int i = 1; i < count; i++)
+        {
+            gameStamps[i] = GenerateGameStamp(rand,
+                gameStamps[i - 1],
+                maxOffsetStep,
+                scoreChangeProbability,
+                homeScoreProbability);
+
+            if (definedOffsetsCurrentIndex < definedOffsets.Length
+                && definedOffsets[definedOffsetsCurrentIndex] <= gameStamps[i].offset)
+            {
+                gameStamps[i].offset = definedOffsets[definedOffsetsCurrentIndex];
+                definedOffsetsCurrentIndex++;
+            }
         }
 
         return gameStamps;
